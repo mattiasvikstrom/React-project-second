29994689fe744e27acdd542bc0a10b35f76ba6b7
mattias vikström <mattias@dragnadrag.se>
2021-09-21 10:17:00 +0200

    new implementations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import './App.css';
-import Navbar from './components/Navigation/Navigation';
-import Home from './components/Home/Home';
-import AboutView from './components/AboutView/AboutView';
-import Products from './components/Products/Products'
-import { Switch, Route } from 'react-router-dom';
+import "./App.css";
+import { useState, useEffect, useContext } from "react";
+import { Switch, Route } from "react-router-dom";
+import { UserContext, UserProvider } from "./shared/global/provider/UserProvider";
+import Navbar from "./components/Navigation/Navigation";
+import Home from "./components/Home/Home";
+import AboutView from "./Views/AboutView/AboutView";
+import Products from "./components/Products/Products";
+import Search from "./components/Search/Search";
+import MovieView from "./Views/MovieView/MovieView";
+import { SignInView } from "./Views/SignInView/SignInView";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("Avengers");
+
+  useEffect(() => {
+    if(searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=3255eeaf8a6e0ad9884eca1fe75a2ba7&language=en-US&query=${searchText}&page=1&include_adult=false`)
+        .then(response => response.json())
+        .then(data => {
+          setSearchResults(data.results)
+        })
+    }
+  }, [searchText])
+
   return (
     <div>
-      <Navbar />
+      <UserProvider>
+      <Navbar searchText={searchText} setSearchText={setSearchText} />
       <Switch>
         <Route path="/" exact>
           <Home />
         </Route>
         <Route path="/about" component={AboutView} />
         <Route path="/products" component={Products} />
+        <Route path="/signin" component={SignInView} />
+        <Route path="/search">
+          <Search keyword={searchText} searchResults={searchResults} />
+        </Route>
+        <Route path="/movies/:id" component={MovieView} />
       </Switch>
+      </UserProvider>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Hero from '../Hero/Hero'
+import { Link } from 'react-router-dom';
+
+const MovieCard = ({ movie }) => {
+    const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    const detailUrl = `/movies/${movie.id}`
+    return (
+        <div className="col-lg-3 col-md-4 my-4">
+            <div className="card">
+            <img src={posterUrl} class="card-img-top" alt={movie.original_title} />
+            <div className="card-body">
+            <h5 className="card-title">{movie.original_title}</h5>
+            <Link to={detailUrl} className="btn btn-primary">Details</Link>
+            </div>
+        </div>
+        </div>
+  )
+}
+
+const Search = ({keyword, searchResults}) => {
+    const title = `You searched for ${keyword}`
+    const resultsHtml = searchResults.map((obj, i) => {
+        return <MovieCard movie={obj} key={i} />
+    })
+
+    return(
+        <>
+            <Hero text={title} />
+            {resultsHtml && 
+            <div className="container">
+                <div className="row">
+                    {resultsHtml}
+                </div>
+            </div>
+            }
+        </>
+    );
+}
+export default Search;
